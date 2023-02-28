51995d08787146105dff4158475c8c1775e93ac1
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-03-01 02:34:05 +0900

    Add files via upload

--- src/D4/P5120_암호.java
+++ src/D4/P5120_암호.java
@@ -15,18 +15,29 @@ public class P5120_암호 {
 			int N = sc.nextInt(); // 숫자 개수
 			int M = sc.nextInt(); // M번째에 칸 추가
 			int K = sc.nextInt(); // 작업 반복 횟수
+			// 숫자 N개 list에 입력받기
 			for (int i = 0; i < N; i++) {
 				list.add(sc.nextInt());
 			}
 
-			for (int i = 1; i <= K; i++) {
-				list.add(K * i % N, list.get((K * i - 1) % N) + list.get((K * i) % N));
-				System.out.println(list);
+			int idx = 0;
+			for (int i = 0; i < K; i++) { // 작업 K번 반복
+				idx += M; // M칸 이동
+				if (idx == list.size()) {
+					// 밀려난 칸 없으면 시작 숫자와 더하기
+					list.add(idx, list.get(idx - 1) + list.get(0));
+				} else {
+					// 밀려난 칸 있으면 앞칸 숫자와 더하기
+					idx %= list.size(); // 리스트 크기 벗어나면 다시 앞(=0)으로 돌아감
+					list.add(idx, list.get(idx-1) + list.get(idx));
+				}
 			}
-			
+
 			System.out.print("#" + tc);
-			for (int i = list.size() - 1; i >= list.size() - 11; i--) {
+			for (int i = list.size() - 1; i >= 0; i--) {
 				System.out.print(" " + list.get(i));
+				if (i == list.size() - 10) // 숫자 10개 이상이면 10개까지만 출력
+					break;
 			}
 			System.out.println();
 		} // testcase
