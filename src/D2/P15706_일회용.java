3b05a248c42998eb1110ed29049f021be36eebbe
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-03-01 15:42:18 +0900

    Add files via upload

--- src/D2/P15706_일회용.java
+++ src/D2/P15706_일회용.java
@@ -24,7 +24,6 @@ public class P15706_일회용 {
 			// N명의 어학 성적 입력받기
 			for (int i = 0; i < N; i++) {
 				score[i] = sc.nextInt();
-
 			}
 
 			// 성적 오름차순으로 정렬
@@ -39,47 +38,53 @@ public class P15706_일회용 {
 				}
 			}
 
-			// 처음에는 T1을 전체 성적 중 최소값, T2를 전체 성적 중 최대값으로 설정.
-			// 단, C분반 나눌 때 T1미만이 아닌, T1 이하로 생각하자.
-			// B반은 T1초과 T2 미만
-			int T1 = cut.get(0);
-			int T2 = cut.get(cut.size() - 1);
+
 			// 제일 신입 사원이 많은 분반과 가장 적은 분반의 차의 최소값
-			int minD = 0;
+			int minD = 1000;
+			// 분반 별 최소 인원, 최대 인원을 만족하는 T1,T2가 있는지 여부. 일단 없다고 가정 -> 있으면 true로 바꿔주기
+			boolean flag = false; 
 
-			// 분반별 최소 및 최대 인원 조건 만족하는지 확인
-			while (true) {
-				// A, B, C 반의 인원
-				int A = 0;
-				int B = 0;
-				int C = 0;
-				// A,B,C반 인원 세보기
-				for (int i = 0; i < N; i++) {
-					if (score[i] <= T1) {
-						C++;
-					} else if (score[i] >= T2) {
-						A++;
-					} else {
-						B++;
+			// 가능한 모든 T1, T2 따져보기. C분반이 T1미만이니까 C분반에 사람 있으려면 T1은 cut 리스트에서 2번째, 즉 1번 인덱스부터 탐색 시작해야 함.
+			for (int i = 1; i < cut.size() - 1; i++) {
+				for (int j = i + 1; j < cut.size(); j++) {  // T2는 T1보다 적어도 1 커야 함
+					int T1 = cut.get(i);
+					int T2 = cut.get(j);
+					// 분반별 최소 및 최대 인원 조건 만족하는지 확인
+
+					// A, B, C 반의 인원
+					int A = 0;
+					int B = 0;
+					int C = 0;
+					// A,B,C반 인원 세보기
+					for (int s = 0; s < N; s++) {
+						if (score[s] < T1) {
+							C++;
+						} else if (score[s] >= T2) {
+							A++;
+						} else {
+							B++;
+						}
+					}
+
+					// 최소, 최대 인원 만족하면 가장 가장 많은 분반과 적은 분반 차이 구하기 + 최소값 갱신
+					if (A >= K_MIN && A <= K_MAX && B >= K_MIN && B <= K_MAX && C >= K_MIN && C <= K_MAX) {
+						int max = Math.max(Math.max(A, B), C);
+						int min = Math.min(Math.min(A, B), C);
+						if (max - min < minD) {
+							minD = max - min;
+						}
+						// 조건 만족하는 T1, T2 있음을 표시
+						flag = true;
 					}
 				}
-
-				// 최소, 최대 인원 만족하면 가장 가장 많은 분반과 적은 분반 차이 구하기 + 최소값 갱신
-				if (A >= K_MIN && A <= K_MAX && B >= K_MIN && B <= K_MAX && C >= K_MIN && C <= K_MAX) {
-					int max = Math.max(Math.max(A, B), C);
-					int min = Math.min(Math.min(A, B), C);
-					if (max - min < minD) {
-						minD = max - min;
-					}
-				}
-
-				// T1, T2 바꿔주기
-				T1++;
-				T2--;
-
 			}
 
-//			System.out.printf("#%d %d", tc, minD);
+			if (flag) { // 조건 만족하는 T1, T2 있으면 가장 많은 분반과 가장 적은 분반 사람 차이 출력
+				System.out.printf("#%d %d\n", tc, minD);
+			} else { // 조건 만족하는 T1, T2 없으면 -1 출력
+				System.out.printf("#%d %d\n", tc, -1);
+			}
+
 		} // testcase
 	} // main
 }
