a9ed7e397955763ba961ea351b209fb45c9141d7
HEOYUJUNG <122508448+HEOYUJUNG@users.noreply.github.com>
2023-03-30 23:36:10 +0900

    Update P1238_Contact.java

--- src/D4/P1238_Contact.java
+++ src/D4/P1238_Contact.java
@@ -1,6 +1,8 @@
 package D4;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class P1238_Contact {
@@ -9,25 +11,26 @@ public class P1238_Contact {
 	static int maxDepth;
 
 	public static void main(String[] args) {
-		for (int tc = 1; tc <= 1; tc++) {
-			Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		for (int tc = 1; tc <= 10; tc++) {
 			int L = sc.nextInt(); // 입력 받는 데이터의 길이
 			int start = sc.nextInt(); // 시작점
-
+			
 			contact = new ArrayList[101]; // 부여될 수 있는 번호는 1이상, 100이하
 			visited = new int[101];
 			for (int i = 1; i <= 100; i++) {
-				contact[i] = new ArrayList<>(); // 전부 초기화 해주기
+				contact[i] = new ArrayList<Integer>(); // 전부 초기화 해주기
 			}
 
 			// 비상연락망 정보 입력받기
-			for (int i = 0; i < L; i++) {
+			for (int i = 0; i < (L / 2); i++) {
 				int from = sc.nextInt();
 				int to = sc.nextInt();
 				contact[from].add(to);
 			} // 입력 끝
 
-			BFS(start, 0);
+			maxDepth = 0;
+			BFS(start, 1);
 
 			int max = 0;
 			for (int i = 1; i <= 100; i++) {
@@ -35,19 +38,42 @@ public class P1238_Contact {
 					max = (max < i) ? i : max;
 				}
 			}
-			
-			System.out.println(max);
+
+			System.out.println("#" + tc + " " + max);
 		} // testcase
 	} // main
 
-	private static void BFS(int x, int depth) {
-		visited[x] = depth;
+	private static void BFS(int st, int depth) {
+		visited[st] = depth;
+		maxDepth = depth;
 
-		maxDepth = (maxDepth < depth) ? depth : maxDepth;
+		Queue<Node> q = new LinkedList<>();
+		q.add(new Node(st, depth));
 
-		for (int n : contact[x]) {
-			if (visited[n] == 0) // 방문한 적 없으면!
-				BFS(x, depth + 1);
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+
+			for (int next : contact[cur.n]) {
+				if (visited[next] == 0) {// 방문한 적 없으면!
+					visited[next] = cur.depth + 1; // 방문처리
+					maxDepth = (maxDepth < cur.depth + 1) ? cur.depth + 1 : maxDepth; // 최대 깊이 갱신
+					q.add(new Node(next, cur.depth + 1)); // 큐에 들어갈 때가 방문한 경우!
+				}
+			}
+		}
+	}
+
+	private static class Node {
+		int n, depth;
+
+		public Node(int n, int depth) {
+			this.n = n;
+			this.depth = depth;
+		}
+
+		@Override
+		public String toString() {
+			return "Node [n=" + n + ", depth=" + depth + "]";
 		}
 
 	}
